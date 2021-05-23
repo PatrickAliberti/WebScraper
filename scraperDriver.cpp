431d98ab27b19bc8f310b46fd103d8657663248a
Patrick Aliberti <56771699+PatrickAliberti@users.noreply.github.com>
2021-05-23 10:15:18 -0400

    Update scraperDriver.cpp

--- scraperDriver.cpp
+++ scraperDriver.cpp
@@ -15,30 +15,34 @@ using namespace std;
 
 int main(void)
 {
-	CURL* curl;							// Handle to a transfer
-    FILE* fp;							// Handle to a file
-    CURLcode res;						// For performing transfers
+	CURL* curl;						// Handle to a transfer
+	FILE* fp;						// Handle to a file
+	CURLcode res;						// For performing transfers
 	
 	WebPage stockPage;
+	
+	// Get the url of the web page depending on the stock symbol entered by user
 	stockPage.getUrl();
+	
+	// Use the url to create a transfer and write the HTML code to a file
 	stockPage.urlToFile(curl, fp, res);
     
-    File htmlFile;
-    
-    // Read the file into a sting so that it can be parsed
-    string file = htmlFile.readFileIntoString();
-    cout << "\nThe HTML code:\n\n" << file << endl;
-    
-    // St the value of the search criteria
-    htmlFile.setSearch("<b>");
-	 
-    // Find where in the file the stock price is
-    unsigned long long position = htmlFile.findPrice();
-    cout << "\nThe index of the price in the file is " << position << endl;
-    
-    // Trim the string so that it only shows the data to be displayed
-    string stockPrice = htmlFile.trimString();
-    cout << "\nThe price of the stock is $" << stockPrice << endl;
-    
-    return 0;
+	File htmlFile;
+
+	// Read the file into a sting so that it can be parsed
+	string file = htmlFile.readFileIntoString();
+	cout << "\nThe HTML code:\n\n" << file << endl;
+
+	// St the value of the search criteria
+	htmlFile.setSearch("<b>");
+
+	// Find where in the file the stock price is
+	unsigned long long position = htmlFile.findPrice();
+	cout << "\nThe index of the price in the file is " << position << endl;
+
+	// Trim the string so that it only shows the data to be displayed
+	string stockPrice = htmlFile.trimString();
+	cout << "\nThe price of the stock is $" << stockPrice << endl;
+
+	return 0;
 }
